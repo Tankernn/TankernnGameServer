59771dba8e3e62684948ea2a85499f5cb93d6c32
Tankernn <frans.bergman@bomlyckan.se>
2016-11-20 21:04:26 +0100

    Initial commit with discard server.

---
+++ src/main/java/eu/tankernn/game/server/GameServerHandler.java
@@ -0,0 +1,22 @@
+package eu.tankernn.game.server;
+
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ExceptionEvent;
+import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.channel.SimpleChannelHandler;
+
+public class GameServerHandler extends SimpleChannelHandler {
+	
+	@Override
+	public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
+		
+	}
+	
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) throws Exception {
+		e.getCause().printStackTrace();
+		Channel ch = e.getChannel();
+		ch.close();
+	}
+}
